762efa2506175c1830ffe75a8852a9f684bd0e18
Jaime Hernandez <jaimevazz@gmail.com>
2021-01-14 00:12:55 -0600

    Added common utility scripts

---
+++ Assets/Utils/BillboardSprite.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class BillboardSprite : MonoBehaviour 
+{
+	[SerializeField] private bool useMainCamera = true;
+	[SerializeField] private bool isCameraMoving = false;
+	[SerializeField] private new Camera camera = default;
+	private Camera mCamera;
+
+	void Start () 
+	{
+		// Assign correct camera
+		if (useMainCamera)
+			mCamera = Camera.main;
+		else 
+			mCamera = camera;
+		// Billboard
+		transform.LookAt(mCamera.transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// Moving camera (billboard effect)
+		if (isCameraMoving)
+		{
+			transform.LookAt(mCamera.transform.position);
+		}
+	}
+}
---
+++ Assets/Utils/JSONSerializer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+public abstract class JSONSerializer<T> : MonoBehaviour where T : MonoBehaviour
+{    
+    [SerializeField] protected string fileName;
+    string filePath;
+    protected void Awake()
+    {
+        filePath = Path.Combine(Application.persistentDataPath, fileName);
+        Debug.Log(filePath);
+    }
+    protected void SetFilePath(string fileName)
+    {
+        this.fileName = fileName;
+        filePath = Path.Combine(Application.persistentDataPath, fileName);
+        Debug.Log(filePath);
+    }
+    protected void Save(object obj)
+    {
+        string json = JsonUtility.ToJson(obj);
+        File.WriteAllText(filePath, json);
+        Debug.Log(json);
+    }
+    protected U GetFromFileOrCreate<U>(Func<U> createFunction)
+    {        
+        return File.Exists(filePath) ? Load<U>() : createFunction();
+    }
+    protected U Load<U>()
+    {
+        string json = File.ReadAllText(filePath);
+        return JsonUtility.FromJson<U>(json);
+    }
+}
