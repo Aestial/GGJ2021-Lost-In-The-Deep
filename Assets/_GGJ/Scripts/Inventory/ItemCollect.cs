ffc3533cc6f4807ee879f68c2991466ab814acc3
Jaime Hernandez <jaimevazz@gmail.com>
2021-01-30 18:50:26 -0600

    Finished Inventory

---
+++ Assets/_GGJ/Scripts/Inventory/ItemCollect.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCollect : MonoBehaviour
+{
+    [SerializeField] 
+    ItemObject itemObject = default;
+
+    Inventory inventory;
+
+    public void Collect() 
+    {
+        itemObject.Print();
+        inventory.Collect(itemObject);
+    }
+
+    private void Awake()
+    {
+        inventory = FindObjectOfType<Inventory>();
+        inventory.Register(this.itemObject);
+    }
+
+}
