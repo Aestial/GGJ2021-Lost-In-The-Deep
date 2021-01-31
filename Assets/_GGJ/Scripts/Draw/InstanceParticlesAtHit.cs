2aa3c3939bad313e893ab3cf9423e3fe7eab8aef
Jaime Hernandez <jaimevazz@gmail.com>
2021-01-30 22:51:24 -0600

    Added InstanceParticlesAtHit behaviour

---
+++ Assets/_GGJ/Scripts/Draw/InstanceParticlesAtHit.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstanceParticlesAtHit : MonoBehaviour
+{
+    [SerializeField]
+    GameObject particles = default;
+
+    public void InstantiateAtHit(RaycastHit hit)
+    {
+        Quaternion rotation = Quaternion.LookRotation(hit.normal);
+        Instantiate(particles, hit.point, rotation);
+    }
+
+    public void InstantiateAtCollision(Collision collision)
+    {
+        ContactPoint contact = collision.GetContact(0);
+        Quaternion rotation = Quaternion.LookRotation(contact.normal);
+        Instantiate(particles, contact.point, rotation);
+    }
+
+    void Start()
+    {
+        
+    }
+}
